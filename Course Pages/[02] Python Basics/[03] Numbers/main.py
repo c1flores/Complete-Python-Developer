2f6e9d435129d7e388d3ad59c8fd05e5eabf3bd2
Christian Flores <c1flores@ucsd.edu>
2023-12-18 10:52:27 -0800

    Add current progress for 2nd learning module in Python course

---
+++ Course Pages/[02] Python Basics/[03] Numbers/main.py
@@ -0,0 +1,21 @@
+# Fundamental Data Types
+
+# int  3, 4, 5 --> Ex/ 3 + 3 = 9 --> print(3+3) -- > 9 (Add, subtract, multiply, divide)
+# float 
+
+print(type(6))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4)) # 0.5
+
+print(type(10.56))
+
+print(type(20+1.1))
+
+print(type(9.9+1.1))
+
+print(2**3)
+print(2//4)
+print(5//4)
+print(5 % 4)
+print(6 % 4)
