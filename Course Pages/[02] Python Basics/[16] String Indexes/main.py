9e05991a6d7c4ee2db9a2e136cb35fd7aef40908
Christian Flores <c1flores@ucsd.edu>
2023-12-26 11:55:10 -0800

    Add 'String Indexes', 'Immutability', and 'Built-In Functions + Methods to Python Basics learning module

---
+++ Course Pages/[02] Python Basics/[16] String Indexes/main.py
@@ -0,0 +1,8 @@
+selfish = '01234567'
+#          01234567
+
+print(selfish[0])
+print(selfish[07])
+
+# [start:stop:step]
+print(selfish[::-1])
---
+++ Course Pages/[02] Python Basics/[18] Built-In Functions/main.py
@@ -0,0 +1,12 @@
+greet = 'hellloooo'
+print(greet[0:len(greet)])
+
+
+quote = 'to be or not to be'
+quote2 = quote.replace('be', 'me')
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+
+print(quote2)
